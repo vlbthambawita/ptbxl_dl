4697dc571ea325f7d0dd4775f46d61b1bdb2c371
Vajira Thambawita <vajira@simula.no>
2023-03-20 21:54:53 +0100

    added options

---
+++ plmodule.py
@@ -0,0 +1,94 @@
+import lightning.pytorch as pl
+from models import CNN
+import torch.nn as nn
+import torch.optim as optim
+import torch
+
+
+
+
+
+class ECGModel(pl.LightningModule):
+
+    def __init__(self,  
+                optimizer_name = "Adam",
+                optimizer_hparams = {"lr":0.0001},
+                lr_scheduler_hparams = {"step_size": 1},
+                prediction_threshold = 0.5
+                ):
+        
+        super().__init__()
+    
+        # Exports the hyperparameters to a YAML file, and create "self.hparams" namespace
+        self.save_hyperparameters()
+        self.prediction_threshold = prediction_threshold
+
+        self.loss_module = nn.BCELoss() #nn.BCEWithLogitsLoss()
+         
+         # Define the model
+        hidden_dim = 32
+        dropout = 0.2
+        self.model = CNN(12, 5, hidden_dim, dropout)
+
+        #self.training_step_outputs = []
+        self.validation_step_outputs_acc = []
+
+
+    def training_step(self, batch, batch_idx):
+        ecg = batch["ecg"]
+        gt = batch["class"]
+        pred = self.model(ecg)
+        loss = self.loss_module(pred, gt)
+
+        #print("gt=", gt)
+        #print("pred=", pred)
+       # self.training_step_outputs.append(preds)
+
+        acc = ((pred > self.prediction_threshold) == gt).float().mean()
+        #print(ecg.shape)
+        #print("gt_shape=", gt.shape)
+        #print(((pred > 0.5) == gt))
+        self.log("train_loss", loss, prog_bar=True, on_epoch=True, sync_dist=True, logger=True)
+        self.log("train_acc", acc, prog_bar=True, on_epoch=True, sync_dist=True, logger=True)
+        #self.log("train_acc_step", acc)
+        return loss
+
+    def on_train_epoch_end(self):
+        #all_preds = torch.stack(self.training_step_outputs)
+        #print("all_preds=", outputs.shape)
+        pass
+
+
+    def validation_step(self, batch, batch_idx):
+        ecg = batch["ecg"]
+        gt = batch["class"]
+        pred = self.model(ecg)
+        loss = self.loss_module(pred, gt)
+
+        
+        acc = ((pred > 0.5) == gt).float().mean()
+        #print(ecg.shape)
+        #print("gt_shape=", gt.shape)
+        self.log("val_loss", loss, prog_bar=True, on_epoch=True, sync_dist=True, logger=True)
+        self.log("val_acc", acc, prog_bar=True, on_epoch=True, sync_dist=True, logger=True)
+        self.validation_step_outputs_acc.append(acc)
+
+    def on_validation_epoch_end(self):
+        all_acc = torch.stack(self.validation_step_outputs_acc)
+        #print(all_acc.shape)
+        self.log("val_epoch_acc", all_acc.mean())
+
+        self.validation_step_outputs_acc.clear()
+        
+    def test_step(self, batch, batch_idx):
+        ecg = batch["ecg"]
+        gt = batch["class"]
+        pred = self.model(ecg)
+
+    def configure_optimizers(self):
+
+        if self.hparams.optimizer_name == "Adam":
+            optimizer = optim.Adam(self.parameters(), **self.hparams.optimizer_hparams)
+
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, **self.hparams.lr_scheduler_hparams)
+        return [optimizer], [lr_scheduler]
--- train.py
+++ train.py
@@ -1,84 +1,8 @@
-import lightning.pytorch as pl
-# main.py
 from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch.loggers import WandbLogger
 from data import PTBXLDataModule 
-from models import CNN
-import torch.nn as nn
-import torch.optim as optim
 
-# simple demo classes for your convenience
-from lightning.pytorch.demos.boring_classes import DemoModel, BoringDataModule
-from pytorch_lightning.loggers import WandbLogger
-
-
-class ECGModel(pl.LightningModule):
-
-    def __init__(self,  
-                optimizer_name = "Adam",
-                optimizer_hparams = {"lr":0.0001}
-                ):
-        
-        super().__init__()
-    
-        # Exports the hyperparameters to a YAML file, and create "self.hparams" namespace
-        self.save_hyperparameters()
-
-        self.loss_module = nn.BCELoss() #nn.BCEWithLogitsLoss()
-         
-         # Define the model
-        hidden_dim = 32
-        dropout = 0.2
-        self.model = CNN(12, 5, hidden_dim, dropout)
-
-        #self.training_step_outputs = []
-
-
-    def training_step(self, batch, batch_idx):
-        ecg = batch["ecg"]
-        gt = batch["class"]
-        pred = self.model(ecg)
-        loss = self.loss_module(pred, gt)
-
-        #print("gt=", gt)
-        #print("pred=", pred)
-       # self.training_step_outputs.append(preds)
-
-        acc = ((pred > 0.5) == gt).float().mean()
-        #print(ecg.shape)
-        #print("gt_shape=", gt.shape)
-        #print(((pred > 0.5) == gt))
-        self.log("train_loss", loss, prog_bar=True, on_epoch=True, sync_dist=True, logger=True)
-        self.log("train_acc", acc, prog_bar=True, on_epoch=True, sync_dist=True, logger=True)
-        self.log("train_acc_step", acc)
-        return loss
-
-    def on_train_epoch_end(self):
-        #all_preds = torch.stack(self.training_step_outputs)
-        #print("all_preds=", outputs.shape)
-        pass
-
-
-    def validation_step(self, batch, batch_idx):
-        ecg = batch["ecg"]
-        gt = batch["class"]
-        pred = self.model(ecg)
-        loss = self.loss_module(pred, gt)
-
-        
-        acc = ((pred > 0.5) == gt).float().mean()
-        #print(ecg.shape)
-        #print("gt_shape=", gt.shape)
-        self.log("val_loss", loss, prog_bar=True, on_epoch=True, sync_dist=True, logger=True)
-        self.log("val_acc", acc, prog_bar=True, on_epoch=True, sync_dist=True, logger=True)
-        
-    def test_step(self, batch, batch_idx):
-        pass
-
-    def configure_optimizers(self):
-
-        if self.hparams.optimizer_name == "Adam":
-            optimizer = optim.Adam(self.parameters(), **self.hparams.optimizer_hparams)
-        return [optimizer], []
+from plmodule import ECGModel
 
        
 
@@ -87,12 +11,12 @@ class MyLightningCLI(LightningCLI):
         parser.add_argument("--wandb_name", default="")
         parser.add_argument("--wandb_entity", default="simulamet_mlc")
         parser.add_argument("--wandb_project", default="PTBXL_ECG")
-        parser.add_argument("--output_dir", default="output/new")
+        #parser.add_argument("--output_dir", default="output/new")
         #parser.set_defaults({"model.output_dir": "output/test/"})
         
         #parser.set_defaults({"model.config"})
         #parser.link_arguments("trainer.callbacks[" + str(0) +"]", "model.output_dir")
-        #parser.link_arguments("output_dir", "model.output_dir")
+        #parser.link_arguments("output_dir", "trainer.callbacks.init_args.dirpath")
         #parser.link_arguments("wandb_name", "model.wandb_name")
         
     #def add_default_arguments_to_parser(self, parser):
